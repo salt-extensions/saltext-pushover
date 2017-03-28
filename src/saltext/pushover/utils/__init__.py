salt-extensions/saltext-pushover

commit 4c10257cfeeb9f065721b49bec6763061d9ef229

fix: no more test against SYSTEM user
Due to the way windows implements languages, we need to avoid hard
testing on the user's name but rather on the SID.
To do that, all test agains SYSTEM user has been factorized in
win_functions.

src/saltext/pushover/utils/__init__.py
@@ -85,6 +85,12 @@ except ImportError:
     HAS_WIN32API = False
 
 try:
+    import salt.utils.win_functions
+    HAS_WIN32 = salt.utils.win_functions.HAS_WIN32
+except ImportError:
+    HAS_WIN32 = False
+
+try:
     import grp
     HAS_GRP = True
 except ImportError:
@@ -283,12 +289,10 @@ def get_user():
     '''
     if HAS_PWD:
         return pwd.getpwuid(os.geteuid()).pw_name
+    elif HAS_WIN32:
+        return salt.utils.win_functions.get_current_user()
     else:
-        user_name = win32api.GetUserNameEx(win32api.NameSamCompatible)
-        if user_name[-1] == '$' and win32api.GetUserName() == 'SYSTEM':
-            # Make the system account easier to identify.
-            user_name = 'SYSTEM'
-        return user_name
+        raise CommandExecutionError("Required external libraries not found. Need 'pwd' or 'win32api")
 
 
 def get_uid(user=None):

commit d5da897239ab912c5bf9b0f0fea6260c8df0369a

Check salt.utils.win_functions.HAS_WIN32 in salt.utils.get_user
Commit f8c460651fdb ("Fix imports") fixed an import issue introduced in
commit 4c10257cfeeb ("fix: no more test against SYSTEM user") by
removing salt.utils.win_functions.HAS_WIN32 check from
salt/utils/__init__.py and setting HAS_WIN32 to True if
salt.utils.win_functions was successfully imported. However,
salt.utils.win_functions doesn't raise ImportError if required libs
aren't available but sets salt.utils.win_functions.HAS_WIN32 to False in
such a case.

So after commit f8c460651fdb ("Fix imports") salt.utils.HAS_WIN32 is
always true:

    (py2-env) salt-test@tommynaut:~> uname -a
    Linux tommynaut 4.9.11-1-default #1 SMP PREEMPT Sat Feb 18 17:59:27 UTC 2017 (cf9c670) x86_64 x86_64 x86_64 GNU/Linux
    (py2-env) salt-test@tommynaut:~> python
    Python 2.7.13 (default, Jan 03 2017, 17:41:54) [GCC] on linux2
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import salt.utils
    >>> salt.utils.HAS_WIN32
    True

salt.utils.get_user thus calls salt.utils.win_functions.get_current_user
without ensuring that all required libs were imported.

Explicitly check salt.utils.win_functions.HAS_WIN32 in get_user(), so it
properly raises CommandExecutionError if required libs aren't available.

src/saltext/pushover/utils/__init__.py
@@ -290,7 +290,7 @@ def get_user():
     '''
     if HAS_PWD:
         return pwd.getpwuid(os.geteuid()).pw_name
-    elif HAS_WIN32:
+    elif HAS_WIN32 and salt.utils.win_functions.HAS_WIN32:
         return salt.utils.win_functions.get_current_user()
     else:
         raise CommandExecutionError("Required external libraries not found. Need 'pwd' or 'win32api")